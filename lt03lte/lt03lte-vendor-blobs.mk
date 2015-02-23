83bdd8a6379afb9b6ea154ecb32e274375caf2ed
Jake Whatley <jacob.whatley91@gmail.com>
2015-02-23 06:21:47 +0000

    lt03lte: add qc soundfx from hlte

--- lt03lte/lt03lte-vendor-blobs.mk
+++ lt03lte/lt03lte-vendor-blobs.mk
@@ -238,6 +238,10 @@ PRODUCT_COPY_FILES += \
 	vendor/samsung/lt03lte/proprietary/vendor/lib/libdisp-aba.so:system/vendor/lib/libdisp-aba.so \
 	vendor/samsung/lt03lte/proprietary/vendor/lib/libmm-abl.so:system/vendor/lib/libmm-abl.so \
 	vendor/samsung/lt03lte/proprietary/vendor/lib/libmm-abl-oem.so:system/vendor/lib/libmm-abl-oem.so \
-  vendor/samsung/lt03lte/proprietary/lib/libxml2.so:system/lib/libxml2.so \
-  vendor/samsung/lt03lte/proprietary/vendor/lib/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so \
-  vendor/samsung/lt03lte/proprietary/vendor/lib/libwvdrm_L3.so:system/vendor/lib/libwvdrm_L3.so
+	vendor/samsung/lt03lte/proprietary/lib/libxml2.so:system/lib/libxml2.so \
+	vendor/samsung/lt03lte/proprietary/vendor/lib/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so \
+	vendor/samsung/lt03lte/proprietary/vendor/lib/libwvdrm_L3.so:system/vendor/lib/libwvdrm_L3.so \
+	vendor/samsung/lt03lte/proprietary/vendor/lib/libqcbassboost.so:system/vendor/lib/soundfx/libqcbassboost.so \
+	vendor/samsung/lt03lte/proprietary/vendor/lib/libqcvirt.so:system/vendor/lib/soundfx/libqcvirt.so \
+	vendor/samsung/lt03lte/proprietary/vendor/lib/libqcreverb.so:system/vendor/lib/soundfx/libqcreverb.so
+
